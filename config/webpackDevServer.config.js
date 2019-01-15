b148f9d1f2d8dd4eae9e137860112a8edd5ee747
Jairo Campos <jairo.campos@janeirodigital.com>
2019-01-14 18:31:26 -0600

    fixed eslint precommit

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,4 +1,4 @@
-'use strict';
+;
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
