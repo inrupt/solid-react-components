d3ff6c0dd68bc38c457de5e67fd0b1970b763daf
Jairo Campos <jairo.campos@janeirodigital.com>
2019-01-21 10:15:07 -0600

    remove example components

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,3 @@
-import Example from "@components/Example";
-import SecondExample from "@components/SecondExample";
 import { ProviderLogin, withWebId, PrivateRoute } from "@components";
 
-export default Example;
-
-export { SecondExample, ProviderLogin, withWebId, PrivateRoute };
+export { ProviderLogin, withWebId, PrivateRoute };
