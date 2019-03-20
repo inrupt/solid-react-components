77e4ecba1fa42471d3082b70873271f79485aa4b
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-03-20 12:16:57 -0600

    Revert "Merge pull request #40 from Inrupt-inc/feature/change-enzyme"
    This reverts commit f17db8190fd40ee7c0c9c7b20a8b50d20d6d7f9a, reversing
    changes made to 80e78c3da599d514beb129e53609b08fac21a2d2.

--- src/lib/components/Uploader/uploader.component.js
+++ src/lib/components/Uploader/uploader.component.js
@@ -245,7 +245,6 @@ class Uploader extends Component<Props> {
         <input
           ref={this.fileInput}
           type='file'
-          data-testid="input-file"
           className='file-uploader--input'
           onChange={this.onFileChanged}
           style={{ display: 'none' }}
--- src/lib/components/withAuthorization/with-authorization.test.js
+++ src/lib/components/withAuthorization/with-authorization.test.js
@@ -1,39 +1,28 @@
 import React from "react";
 import auth from 'solid-auth-client';
-import { MemoryRouter } from "react-router-dom";
-import { render, cleanup } from 'react-testing-library';
+import { Redirect, MemoryRouter } from "react-router-dom";
+
+import { mount } from "enzyme";
 import { withAuthorization } from "@components";
-import 'jest-dom/extend-expect';
+
+import "@testSetup";
+
 
 const ComponentExample = () => <div>Component Example</div>;
-const ComponentLoader = () => <div>Loader Component</div>;
+const ComponentLoader = () => <div>Loader Example</div>;
 
 describe('A withWebId wrapper', () => {
   const defaultWeb = "https://example.org/#me";
   const Wrapper = withAuthorization(ComponentExample, <ComponentLoader />);
-  const { container } = render(<MemoryRouter><Wrapper/></MemoryRouter>);
+  let wrapper;
 
-  /**
-   *  This is a workaround to avoid console warning message on react-dom.
-   *  If you want to know more about please go to:
-   *  https://github.com/facebook/react/issues/14769#issuecomment-470097212
-   */
-
-  const originalError = console.error;
-
-  beforeAll(() => {
-    console.error = jest.fn();
-  });
-
-  afterAll(() => {
-    console.error = originalError;
-    cleanup();
-  });
-
+  beforeAll(() => (wrapper = mount(<MemoryRouter><Wrapper /></MemoryRouter>)));
+  beforeEach(() => wrapper.update());
+  afterAll(() => wrapper.unmount());
 
   describe('before a session is received', () => {
     it('renders the loader component', () => {
-      expect(container).toHaveTextContent('Loader Component');
+      expect(wrapper.find(ComponentLoader).length).toEqual(1);
     });
   });
 
@@ -41,8 +30,7 @@ describe('A withWebId wrapper', () => {
     beforeAll(() => auth.mockWebId(null));
 
     it('redirect user', () => {
-      expect(container).not.toHaveTextContent('Component');
-      expect(container).not.toHaveTextContent('Loader Component');
+      expect(wrapper.find(Redirect).length).toEqual(1);
     });
   });
 
@@ -50,7 +38,7 @@ describe('A withWebId wrapper', () => {
     beforeAll(() => auth.mockWebId(defaultWeb));
 
     it('renders the wrapped component', () => {
-      expect(container).toHaveTextContent('Component Example');
+      expect(wrapper.find(ComponentExample).length).toEqual(1);
     });
   });
 });
