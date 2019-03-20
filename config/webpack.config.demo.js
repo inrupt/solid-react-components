abd1f634584c7fc5b803e326b12ec6557e9bf1d8
Jairo Campos <jairo.campos@janeirodigital.com>
2019-03-20 10:08:27 -0600

    Fixed webpack configuration

--- config/webpack.config.demo.js
+++ config/webpack.config.demo.js
@@ -213,8 +213,8 @@ module.exports = {
     // for React Native Web.
     extensions: ['.mjs', '.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      react: path.resolve(path.join(__dirname, './node_modules/react')),
-      'react-dom': path.resolve(path.join(__dirname, './node_modules/react-dom')),
+      react: path.resolve(path.join(__dirname, '../node_modules/react')),
+      'react-dom': path.resolve(path.join(__dirname, '../node_modules/react-dom')),
       // 'react-dom': require.resolve('react-dom'),
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
