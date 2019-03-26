d4515e3e1af69506316379bb79d26c2ca26f5b72
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-03-26 15:12:00 -0600

    package.json fix, PrivateRoute addons

--- stories/PrivateRoute/private-route.stories.js
+++ stories/PrivateRoute/private-route.stories.js
@@ -3,5 +3,11 @@ import { storiesOf } from '@storybook/react';
 import {
     PrivateRoute
 } from "@solid-react-components";
+import Readme from "./README.md";
 
-storiesOf('Private Route', module).add('default', () => <h1> Private Route</h1>);
+storiesOf('Private Route', module).addParameters({
+    readme: {
+        // Show readme at the addons panel
+        sidebar: Readme,
+    },
+}).add('default', () => <h1> Private Route</h1>);
