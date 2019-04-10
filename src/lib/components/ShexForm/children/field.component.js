2663bf15940c644866605a9c4376c0dea666dca1
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-04-10 10:06:31 -0600

    field and fields component

--- src/lib/components/ShexForm/children/field.component.js
+++ src/lib/components/ShexForm/children/field.component.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const Field = (props) => {
-    const { data } = props;
-    const label = data.predicate.includes("#") ? data.predicate.split("#")[1] : data.predicate.split("/").pop();
-    const values = data._formValues ? data._formValues.map(value => value._formFocus.value) : [];
-    const inputType = data.valueExpr.values ? 'select' : 'text';
-    console.log("Label", data)
-    return (<div>
-        <label>{label}</label>
-        <ul>{
-            values.map((value,i) => <li key={i}>
-                {inputType === 'text' ? <input type='text' value={value} /> : <select>{data.valueExpr.values.map(value => <option value={value}>{value.split("#")[1]}</option>)}</select>
-                }
-            </li>)
-        }
-        </ul>
-    </div>)
+const Fields = props => {
+  const { data } = props;
+  const label = data.predicate.includes("#")
+    ? data.predicate.split("#")[1]
+    : data.predicate.split("/").pop();
+  const values = data._formValues
+    ? data._formValues.map(value => value._formFocus.value)
+    : [];
+  console.log("Label", data);
+  return (
+    <div>
+      <label>{label}</label>
+      <ul>
+        {values.map((value, i) => (
+          <li key={i}>
+            <Field {...{ data, value }} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-}
+type FieldProps = {
+  data: Object,
+  value: String
+};
 
-export default Field;
+const Field = ({ data, value }: FieldProps) => {
+  const { fieldData, setFieldData } = useState();
+    const inputType = data.valueExpr.values ? "select" : "text";
+    const init = () => {
+    const fieldData = {
+      value,
+      error: null,
+      predicate: "",
+      subject: ""
+    };
+
+    setFieldData(fieldData);
+  };
+
+  useEffect(() => {
+    init();
+  }, [props.formData]);
+
+  return inputType === 'text' ? <input type='text' value={fieldData.value} /> : <select>{data.valueExpr.values.map(value => <option value={value}>{value.split("#")[1]}</option>)}</select>;
+};
+
+//
+//                 }
+
+export default Fields;
