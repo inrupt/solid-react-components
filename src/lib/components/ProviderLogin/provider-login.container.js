7ef788ae8dbb3504199d83c77ecc60b7e62b5dc1
Jamie Fiedler <jamie.fiedler@janeirodigital.com>
2019-04-24 11:05:14 -0400

    update webID validation regex to better match valid http/https URLs
    fixes #56
    
    modified from original source below
    https://gist.githubusercontent.com/dperini/729294/raw/a59e17ecc4f0c7784c84b39b4a8cdd74020e18b0/regex-weburl.js

--- src/lib/components/ProviderLogin/provider-login.container.js
+++ src/lib/components/ProviderLogin/provider-login.container.js
@@ -36,7 +36,7 @@ export default class LoginComponent extends Component<Props> {
 
   isWebIdValid = webId => {
     const regex = new RegExp(
-      /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/,
+      /^(?:(?:https?:)?\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?)|localhost)(?::\d{2,5})?(?:[/?#]\S*)?$/,
       "i",
       "g"
     );
