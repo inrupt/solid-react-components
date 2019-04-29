83cee2534b426f41cb0269b1043d1f0d70eaeb06
Jairo Campos <jairo.campos@janeirodigital.com>
2019-04-29 14:58:22 -0600

    add react hook library

--- src/lib/hooks/useShex.test.js
+++ src/lib/hooks/useShex.test.js
@@ -1,33 +1,21 @@
-import React from 'react';
 import { useShex } from '@hooks';
-import { cleanup, render } from 'react-testing-library';
+import { renderHook } from 'react-hooks-testing-library';
+import { cleanup } from 'react-testing-library';
 
-const Shex = ({children, ...rest}) => children(useShex(rest));
-
-function setup(props) {
-    const returnShex = {};
-    render(
-        <Shex {...props}>
-            {val => {
-                Object.assign(returnShex, val)
-                return null
-            }}
-        </Shex>,
-    )
-    return returnShex
-}
+const setup = () => {
+    return renderHook(() => useShex());
+};
 
 describe('useWebId', () => {
-    let result;
 
     afterAll( () => cleanup);
 
     it('returns object when shexC is not loaded', async () => {
-        const result = await setup();
+        const { result } = setup();
 
-        expect(result.shexData).toEqual({});
-        expect(result.addNewShexField).toBeTruthy();
-        expect(result.updateShexJ).toBeTruthy();
+        expect(result.current.shexData).toEqual({});
+        expect(result.current.addNewShexField).toBeTruthy();
+        expect(result.current.updateShexJ).toBeTruthy();
     });
 
 
