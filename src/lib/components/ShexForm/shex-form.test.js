d5df984d2f62155efd99b11b375757e667c82a50
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-05-01 08:11:36 -0600

    UI test started

--- src/lib/components/ShexForm/shex-form.test.js
+++ src/lib/components/ShexForm/shex-form.test.js
@@ -1,19 +1,57 @@
 import React from "react";
-import { cleanup } from 'react-testing-library';
-// import ShexForm from "./shex-form.component";
-import 'jest-dom/extend-expect';
-
+import { render, cleanup } from "react-testing-library";
+import ShexForm from "./shex-form.component";
+import "jest-dom/extend-expect";
 
 afterAll(cleanup);
 
-
+const shexj = {
+  type: "Shape",
+  expression: {
+    expressions: [
+      {
+        type: "TripleConstraint",
+        predicate: "http://www.w3.org/2006/vcard/ns#fn",
+        valueExpr: {
+          type: "NodeConstraint",
+          datatype: "http://www.w3.org/2001/XMLSchema#string"
+        },
+        annotations: [],
+        _formValues: [
+          {
+            type: "NodeConstraint",
+            datatype: "http://www.w3.org/2001/XMLSchema#string",
+            _formFocus: {
+              value: "Jane",
+              parentSubject: "https://webid/profile/card#me",
+              name: "7bb3baa7ecef1191a73e55a118b5b01a",
+              isNew: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  id: "http://localhost:3000/#UserProfile"
+};
 describe("Shex ShapeForm Component", () => {
-    /* const { container } = render(<ShexForm  />);
+  const onDeleteMock = jest.fn();
+  const onChangeMock = jest.fn();
+  const addNewExpressionMock = jest.fn();
+  const updateShexjMock = jest.fn();
+  const { container  } = render(
+    <ShexForm
+      shexj={shexj}
+      onChange={onChangeMock}
+      onDelete={onDeleteMock}
+      addNewExpression={addNewExpressionMock}
+      updateShexj={updateShexjMock}
+      formValues={{}}
+    />
+  );
 
-    it("shoud renders without crashing", () => {
-        expect(container).not.toBeTruthy();
-    }); */
-
-    it("shoud renders without crashing", () => {});
-
+  it("should renders without crashing", () => {
+    expect(container).toBeTruthy();
+  });
+  
 });
