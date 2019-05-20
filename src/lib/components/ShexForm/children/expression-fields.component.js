aa2e3c2c303f54919d88bd26358201356f53a950
Pablo Rodriguez <pablo.rodriguez@janeirodigital.com>
2019-05-20 11:48:53 -0600

    Input labels

--- src/lib/components/ShexForm/children/expression-fields.component.js
+++ src/lib/components/ShexForm/children/expression-fields.component.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
-import { shexFormLabel, allowNewFields, canDelete } from "@utils";
-import { ShexConfig } from "@context";
+import { allowNewFields, canDelete } from "@utils";
 import { AddButton } from "./";
 import { Field } from "./";
 
@@ -12,37 +11,32 @@ type FieldsProps = {
 
 const ExpressionFields = ({ data, formValues, parent }: FieldsProps) => {
   return (
-    <ShexConfig.Consumer>
-      {({ languageTheme: { language } }) => (
-        <Fragment>
-          <label>{shexFormLabel(data, language)}</label>
-          <ul>
-            {data._formValues &&
-              data._formValues.map((value, i) => (
-                <li key={i}>
-                  <Field
-                    {...{
-                      data,
-                      fieldData: value,
-                      inputData:
-                        formValues[value._formFocus.name] || value._formFocus,
-                      parent,
-                      canDelete: canDelete(data)
-                    }}
-                  />
-                </li>
-              ))}
-          </ul>
-          <AddButton
-            {...{
-              allowNewFields: allowNewFields(data) && !parent,
-              defaultExpression: data,
-              expression: parent
-            }}
-          />
-        </Fragment>
-      )}
-    </ShexConfig.Consumer>
+    <Fragment>
+      <ul>
+        {data._formValues &&
+          data._formValues.map((value, i) => (
+            <li key={i}>
+              <Field
+                {...{
+                  data,
+                  fieldData: value,
+                  inputData:
+                    formValues[value._formFocus.name] || value._formFocus,
+                  parent,
+                  canDelete: canDelete(data)
+                }}
+              />
+            </li>
+          ))}
+      </ul>
+      <AddButton
+        {...{
+          allowNewFields: allowNewFields(data) && !parent,
+          defaultExpression: data,
+          expression: parent
+        }}
+      />
+    </Fragment>
   );
 };
 
