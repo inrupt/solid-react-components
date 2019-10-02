71fa7d496c2bdfb79aacc78f3441f904db8dff0f
Jairo  Campos <jairo.campos@janeirodigital.com>
2019-10-02 13:34:08 -0600

    added class on buttons and package version change

--- src/lib/components/FormModel/children/Form/UI/DeleteButton/delete-button.component.js
+++ src/lib/components/FormModel/children/Form/UI/DeleteButton/delete-button.component.js
@@ -3,12 +3,13 @@ import React from 'react';
 type Props = {
   action: id => void,
   id: string,
-  text: string
+  text: string,
+  className: string
 };
 
-export const DeleteButton = ({ action, text = 'Delete', type, id }: Props) =>
+export const DeleteButton = ({ action, text = 'Delete', type, id, className }: Props) =>
   type.includes('Group') && (
-    <button type="button" onClick={() => action(id)}>
+    <button type="button" onClick={() => action(id)} className={className}>
       {text}
     </button>
   );
