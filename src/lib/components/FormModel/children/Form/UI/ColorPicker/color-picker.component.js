689920d5a5815e11dd40c6430523b7d230b3830b
Angel Araya <angel.araya@janeirodigital.com>
2019-12-04 07:45:59 -0600

    Initial color picker implementation

--- src/lib/components/FormModel/children/Form/UI/ColorPicker/color-picker.component.js
+++ src/lib/components/FormModel/children/Form/UI/ColorPicker/color-picker.component.js
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 import { ChromePicker } from 'react-color';
 
-const ColorPicker = ({ id, modifyFormObject, formObject, ...rest }) => {
-  const [pickerVisible, setPickerVisible] = useState(false);
-  const [color, setColor] = useState(formObject.value || null);
+import { FormModelUI } from '@constants';
+import { PickerGroup, ColorSwatch, Cover, Popover } from './color-picker.styles';
 
-  const handleChange = color => {
+const ColorPicker = ({
+  id,
+  modifyFormObject,
+  formObject,
+  value,
+  onSave,
+  autosave,
+  onBlur,
+  ...rest
+}) => {
+  let actualValue = formObject[id] || formObject[id] === '' ? formObject[id].value : value;
+
+  /* default value for the color */
+  if (!actualValue) actualValue = '#3498ef';
+
+  const [pickerVisible, setPickerVisible] = useState(false);
+  const [color, setColor] = useState(actualValue);
+
+  const label = rest[FormModelUI.UI_LABEL] || '';
+  const handleChange = (color, event) => {
+    event.preventDefault();
     setColor(color.hex);
   };
 
-  const handleChangeComplete = color => {
-    setColor(color.hex);
-    const obj = { value: color.hex, ...rest };
+  const handleChangeComplete = (color, event) => {
+    event.preventDefault();
+    const obj = { ...rest, value: color.hex };
     modifyFormObject(id, obj);
+    onSave();
+    setColor(color.hex);
   };
 
   const handleClick = () => {
@@ -23,35 +44,22 @@ const ColorPicker = ({ id, modifyFormObject, formObject, ...rest }) => {
     setPickerVisible(false);
   };
 
-  const popover = {
-    position: 'absolute',
-    zIndex: '2'
-  };
-  const cover = {
-    position: 'fixed',
-    top: '0px',
-    right: '0px',
-    bottom: '0px',
-    left: '0px'
-  };
-
   return (
-    <div>
-      <button type="button" onClick={handleClick}>
-        {color ? JSON.stringify(color) : 'Pick Color'}
-      </button>
+    <PickerGroup>
+      <label htmlFor={id}>{label}</label>
+      <ColorSwatch color={color} onClick={handleClick} />
       {pickerVisible ? (
-        <div style={popover}>
-          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-          <div style={cover} onClick={handleClose} />
+        <Popover>
+          <Cover onClick={handleClose} />
           <ChromePicker
+            id
             color={color}
             onChangeComplete={handleChangeComplete}
             onChange={handleChange}
           />
-        </div>
+        </Popover>
       ) : null}
-    </div>
+    </PickerGroup>
   );
 };
 
