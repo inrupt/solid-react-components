e22f87042ce04f79dc411e8e8829275ae0ba0c1e
Angel Araya <angel.araya@janeirodigital.com>
2019-12-05 13:25:35 -0600

    SingleLine: default to empty string when no value is found

--- src/lib/components/FormModel/children/Viewer/UI/SingleLine/single-line.component.js
+++ src/lib/components/FormModel/children/Viewer/UI/SingleLine/single-line.component.js
@@ -5,16 +5,16 @@ import { Wrapper, Label, Value } from './single-line.style';
 import { FormModelUI } from '@constants';
 
 const SingleLine = ({ value, ...rest }: { value: String }) => {
-  return value ? (
+  return (
     <FormModelConfig.Consumer>
       {({ theme }) => (
         <Wrapper className={theme && theme.singleLineViewerClass}>
           <Label className="label">{rest[FormModelUI.UI_LABEL]}</Label>
-          <Value className="value">{value}</Value>
+          <Value className="value">{value || ''}</Value>
         </Wrapper>
       )}
     </FormModelConfig.Consumer>
-  ) : null;
+  );
 };
 
 export default SingleLine;
