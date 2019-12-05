0d51b87d35331f8d1d8e0086e62a6b48ce23d419
Angel Araya <angel.araya@janeirodigital.com>
2019-12-05 13:29:26 -0600

    Multiline: default to empty string when value is missing

--- src/lib/components/FormModel/children/Viewer/UI/MultiLine/multi-line.component.js
+++ src/lib/components/FormModel/children/Viewer/UI/MultiLine/multi-line.component.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { FormModelConfig } from '@context';
 import { Wrapper, Label, Value } from './multi-line.style';
 
+import { FormModelConfig } from '@context';
+import { FormModelUI } from '@constants';
+
 const MultiLine = ({ value, ...rest }: { value: String }) => {
-  return value ? (
+  return (
     <FormModelConfig.Consumer>
       {({ theme }) => (
         <Wrapper className={theme && theme.multiLineViewerClass}>
-          <Label className="label">{rest['ui:label']}</Label>
-          <Value className="value">{value}</Value>
+          <Label className="label">{rest[FormModelUI.UI_LABEL]}</Label>
+          <Value className="value">{value || ''}</Value>
         </Wrapper>
       )}
     </FormModelConfig.Consumer>
-  ) : null;
+  );
 };
 
 export default MultiLine;
