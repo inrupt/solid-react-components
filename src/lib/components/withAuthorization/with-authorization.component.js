29ed1d79c4e8b83aa53e7d3bb337a7c0966f391e
James Martin <james.martin@janeirodigital.com>
2020-03-04 11:19:03 -0500

    Adding clarifying comments to non-authorized redirect

--- src/lib/components/withAuthorization/with-authorization.component.js
+++ src/lib/components/withAuthorization/with-authorization.component.js
@@ -10,6 +10,9 @@ export const withAuthorization = (Component, Loader) =>
           case undefined:
             return Loader || null;
           case null:
+            // Using the non-SPA redirect here to clear the state when the user is not logged in
+            // This helps with making sure state is fully clean on login, and addresses an issue with
+            // the react-router-dom v5 upgrade, which didn't like using <Redirect> here
             window.location.href = '/login';
             return null;
           default:
